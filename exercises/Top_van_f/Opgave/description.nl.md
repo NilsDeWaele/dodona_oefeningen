dcbd5564a3b6854456dc5cbc4d6190e8c93cfb4d
NilsDeWaele <148898452+NilsDeWaele@users.noreply.github.com>
2023-10-24 20:39:14 +0200

    voeg mijn eerste oefening toe

---
+++ exercises/Top_van_f/Opgave/description.nl.md
@@ -0,0 +1,2 @@
+Schrijf een code die de top van een grafiek van een tweedegraadsfunctie kan berekenen. 
+Hierbij heb je drie inputs nodig, dit zijn a, b en c; respectievelijke coëfficiënten in de algemene tweedegraadsfunctie ax^2+bx+c.
---
+++ exercises/Top_van_f/Solution/solution.py
@@ -0,0 +1,8 @@
+a = float(input("Voer de coëfficiënt van x^2 in."))
+b = float(input("Voer de coëfficiënt van x in."))
+c = float(input("Voer de term zonder x in."))
+
+alpha = (-b)/(2*a)
+beta = (-b**2+4*a*c)/(4*a)
+
+print("De coördinaat van de top van de grafiek van f is: T(",alpha;";",beta,")")
