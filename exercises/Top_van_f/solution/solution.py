bf1481c701d8edf4aa8de80287fb616ac8436417
NilsDeWaele <148898452+NilsDeWaele@users.noreply.github.com>
2023-10-24 20:54:24 +0200

    l

--- exercises/Top_van_f/solution/solution.py
+++ exercises/Top_van_f/solution/solution.py
@@ -5,4 +5,4 @@ c = float(input("Voer de term zonder x in."))
 alpha = (-b)/(2*a)
 beta = (-b**2+4*a*c)/(4*a)
 
-print("De coördinaat van de top van de grafiek van f is: T(",alpha;";",beta,")")
+print("De coördinaat van de top van de grafiek van f is: T(",alpha,";",beta,")")
